d885616c5e39529ee29cf272345ac484111f5000
MichaelBonsall <104641118+MichaelBonsall@users.noreply.github.com>
2024-09-08 18:58:36 -0700

    Update DecryptMessage.cpp

--- DecryptMessage.cpp
+++ DecryptMessage.cpp
@@ -4,10 +4,12 @@
 #include "RSA.cpp"
 
 
-static const string cipherText = "22573068466540092756306013238544216912 10321392066240359832073445998216514622 4566514889366191369752423369763713611 4450519335398492259715717342376536982 4450519335398492259715717342376536982 15054423825242970548392889601717131632 22397307351322846527199535759111205228 18109120169981032842842605665273815525" ;
-static const string decryptionKey = "9595502799659191645956353566665847089";
-static const string modulo = "23503530684005996531166929758775217061";
-
+/**
+ * @brief Removes spaces from input string
+ * 
+ * @param input The string you want to remove spaces from
+ * @return string with no spaces
+ */
 string removeSpaces(const string& input) {
     std::string result;
     for (char c : input) {
@@ -18,7 +20,14 @@ string removeSpaces(const string& input) {
     return result;
 }
 
-
+/**
+ * @brief Decrypt an encrypted message. Give the other party the encryption key and the modulus. No need to worry about BLOCKSIZE since the spaces are the blocks.  
+ * 
+ * @param cipherText The ciphertext you want to decrypt
+ * @param modulo The modulo given by RSAKeyGenerator()
+ * @param decryptionKey  The decryption key given by RSAKeyGenerator()
+ * @return The decrypted message
+ */
 string decryptMessage(string cipherText, string modulo, string decryptionKey){
     string ascii = "";
     string message = "";
@@ -35,16 +44,16 @@ string decryptMessage(string cipherText, string modulo, string decryptionKey){
         /*
         Since leading zeroes are stripped in string math, sometimes they needed to be added back to convert back to ASCII properly.
 
-        126 is the highest ASCII code that can be used, so anything higher must be another character thats missing a 0.  
+        126 is the highest ASCII code that is a normal character, so anything higher must be another character thats missing a 0. Thus, we add it back
         */
         if (stoi(messageBlock.substr(0, 3)) > 126 ) messageBlock = "0" + messageBlock; 
         ascii = ascii + messageBlock + " ";
         
     }
     
-    //cout<<ascii<<endl;
+
     ascii = removeSpaces(ascii);
-    for (int i = 0; i < ascii.length(); i += 3) {  // Assuming ASCII values are 3 digits long
+    for (int i = 0; i < ascii.length(); i += 3) {  // Assuming ASCII values are 3 digits long, which they should be thanks to step above
         string asciiValue = ascii.substr(i, 3);
         message += (char)stoi(asciiValue);
     }
@@ -52,7 +61,4 @@ string decryptMessage(string cipherText, string modulo, string decryptionKey){
     return message;
 }
 
-int main(){
 
-    cout<<decryptMessage(cipherText, modulo,  decryptionKey);
-}
